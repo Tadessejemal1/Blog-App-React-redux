a7544e29ad1c2002b50b29a724f92b9ac1a64dc0
Tadesse Jemal <tadessejemal63@gmail.com>
2023-03-15 11:36:10 +0300

    update configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,10 +2,15 @@
 /* eslint-disable no-undef */
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../redux/counter/counterSlice';
+import postReducer from '../redux/counter/postSlice';
+import usersReducer from '../redux/counter/userSlice';
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,
+        post: postReducer,
+        users: usersReducer
+
     }
 })
 
