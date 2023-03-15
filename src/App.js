f667dac7bdc1608b9211c0bcdcd360de4d582792
Tadesse Jemal <tadessejemal63@gmail.com>
2023-03-15 11:36:25 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Counter from './components/counter';
+import Post from './components/AddPostForm';
+import PostList from './components/PostList';
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <Post />
+      <PostList />
     </div>
   );
 }
