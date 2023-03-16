68bc28958e56f5060460c79b916d3d5bc97bbc6e
Tadesse Jemal <tadessejemal63@gmail.com>
2023-03-16 16:30:01 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,24 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/configureStore';
 import { Provider } from 'react-redux';
+import { fetchUsers } from './redux/counter/userSlice';
+import { fetchPosts } from './redux/counter/postSlice';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+store.dispatch(fetchUsers());
+store.dispatch(fetchPosts());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <Provider store={store}>
-    <App />
-   </Provider> 
-  </React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
