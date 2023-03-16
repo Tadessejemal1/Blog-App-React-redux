43b6fdae99d50f9a423d12a3ac63681d8c8517c4
Tadesse Jemal <tadessejemal63@gmail.com>
2023-03-16 16:25:04 +0300

    update userSlice

--- src/redux/counter/userSlice.js
+++ src/redux/counter/userSlice.js
@@ -1,15 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-    { id: '0', name: 'Dude Lebowski' },
-    { id: '1', name: 'Neil Young' },
-    { id: '2', name: 'Dave Gray' }
-]
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {    
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+    return response.data
+})
 
 const usersSlice = createSlice({
     name: 'users',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers (builder) {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            return action.payload
+        })
+    }
 })
 
 export const selectAllUsers = (state) => state.users;
