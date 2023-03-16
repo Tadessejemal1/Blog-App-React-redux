85d77dbec667fe3b40511474c420b22695181ba6
Tadesse Jemal <tadessejemal63@gmail.com>
2023-03-16 16:29:37 +0300

    Update App.js by adding routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.css';
-import Counter from './components/counter';
-import Post from './components/AddPostForm';
+import AddPostForm from './components/AddPostForm';
+import { Routes, Route } from 'react-router-dom';
+import SinglePostPage from "./components/SinglePostPage"
+import Layout from './components/Layout';
 import PostList from './components/PostList';
+import EditPostForm from './components/EditPostForm';
 
 function App() {
   return (
-    <div className="App">
-      <Counter />
-      <Post />
-      <PostList />
-    </div>
+    <Routes>
+      <Route path = '/' element = {<Layout />}>
+        <Route path = '/' element = {<PostList />} />
+
+        <Route path = './post'>
+          <Route index element = {<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
+        </Route>
+      </Route>  
+    </Routes>
   );
 }
 
