a1039cfe5e57807e4e8eaa6fec73fff64eaaf3b0
Tadesse Jemal <tadessejemal63@gmail.com>
2023-03-16 16:27:14 +0300

    Update Form

--- src/components/AddPostForm.js
+++ src/components/AddPostForm.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
-import { postAdded } from "../redux/counter/postSlice";
+import { addNewPost } from "../redux/counter/postSlice";
 import { selectAllUsers } from "../redux/counter/userSlice";
 
 const AddPostForm = () => {
     const dispatch = useDispatch()
 
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
-    const [userId, setUserId] = useState('')
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [userId, setUserId] = useState('');
+    const [addRequestStatus, setAddRequestStatus] = useState('idle');
 
     const users = useSelector(selectAllUsers)
 
@@ -17,17 +17,23 @@ const AddPostForm = () => {
     const onContentChanged = e => setContent(e.target.value)
     const onAuthorChanged = e => setUserId(e.target.value)
 
+    const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
     const onSavePostClicked = () => {
-        if (title && content) {
-            dispatch(
-                postAdded(title, content, userId)
-            )
-            setTitle('')
-            setContent('')
+        if (canSave) {
+            try {
+                setAddRequestStatus('pending')
+                dispatch(addNewPost({ title, content, userId })).unwrap()
+                setTitle('')
+                setContent('')
+                setUserId('')
+            } catch (err) {
+                console.error('Failed to save the post: ', err)
+            } finally {
+                setAddRequestStatus('idle')
+            }
         }
     }
 
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
     const usersOptions = users.map(user => (
         <option key={user.id} value={user.id}>
