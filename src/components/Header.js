aac018c787be3cea45ef8a4bef00ff849f71268c
Tadesse Jemal <tadessejemal63@gmail.com>
2023-03-18 16:35:08 +0300

    update header components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom"
 
 const Header = () => {
     return (
-        <header className = 'header'>
-            <h1>Blog Post</h1>
+        <header className="Header">
+            <h1>Redux Blog</h1>
             <nav>
                 <ul>
-                    <li><Link to = '/'>Home</Link></li>
-                    <li><Link to = '/post'>Post</Link></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="post">Post</Link></li>
                 </ul>
             </nav>
         </header>
-    );
+    )
 }
 
-export default Header;
+export default Header
