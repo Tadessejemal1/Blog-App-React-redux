8480e4c37dfad93a9f823b937fbe8b0e3257de9c
Tadesse Jemal <tadessejemal63@gmail.com>
2023-03-18 16:34:48 +0300

    update layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,11 @@
-import { Outlet } from 'react-router-dom'
-import Header from './Header'
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
 
 const Layout = () => {
     return (
         <>
             <Header />
-            <main>
+            <main className="App">
                 <Outlet />
             </main>
         </>
