957a91742b3d46171f02d05d2a48b8e3a988c9cc
Tadesse Jemal <tadessejemal63@gmail.com>
2023-03-18 16:36:25 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import './App.css';
-import AddPostForm from './components/AddPostForm';
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import EditPostForm from "./features/posts/EditPostForm";
+import Layout from "./components/Layout";
 import { Routes, Route } from 'react-router-dom';
-import SinglePostPage from "./components/SinglePostPage"
-import Layout from './components/Layout';
-import PostList from './components/PostList';
-import EditPostForm from './components/EditPostForm';
 
 function App() {
   return (
     <Routes>
-      <Route path = '/' element = {<Layout />}>
-        <Route path = '/' element = {<PostList />} />
+      <Route path="/" element={<Layout />}>
 
-        <Route path = './post'>
-          <Route index element = {<AddPostForm />} />
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
           <Route path=":postId" element={<SinglePostPage />} />
           <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
-      </Route>  
+
+      </Route>
     </Routes>
   );
 }
